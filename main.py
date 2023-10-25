7729f1588f9b239767a75a895b41c7f0062431fd
SongYibao <songybmail@foxmail.com>
2023-10-25 12:42:55 +0800

    sr && 2d done

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 # This is a sample Python script.
 import base64
 
+import torch
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 from flask import Flask, request, send_file, jsonify
-
+# from flask.ext.restful import reqparse, abort, Api, Resource
 from typing import List, Union
 import Final2x_core as Fin
 import os
@@ -13,6 +14,10 @@ import time
 import cv2
 from PIL import Image
 import io
+# -*- coding: utf-8 -*-
+
+from flask_restful import reqparse, Resource, Api
+from werkzeug.datastructures import FileStorage
 
 app = Flask(__name__)
 
@@ -49,14 +54,27 @@ def myupscale(filename):
     return filename
 
 
-@app.route('/upload', methods=['POST'])
-def upload_image():
+def pic2anime(filename):
+    inputs = app.config['UPLOAD_FOLDER']
+    outputs = app.config['OUTPUT_FOLDER']
+    face2paint = torch.hub.load("bryandlee/animegan2-pytorch:main", "face2paint", size=512)
+    model = torch.hub.load("bryandlee/animegan2-pytorch:main", "generator", pretrained="face_paint_512_v2")
+    input_image = os.path.join(inputs, filename)
+    img = Image.open(input_image).convert("RGB")
+    out = face2paint(model, img)
+
+    out.save(os.path.join(outputs, filename))
+    return filename
+
+
+# @app.route('/upload', methods=['POST'])
+def upload_image(file, type):
     inputs = app.config['UPLOAD_FOLDER']
     outputs = app.config['OUTPUT_FOLDER']
     if 'file' not in request.files:
         return "No file part"
 
-    file = request.files['file']
+    # file = request.files['file']
 
     if file.filename == '':
         return "No selected file"
@@ -73,8 +91,11 @@ def upload_image():
         filename = f"{timestamp}_{file.filename}"
         image_path = os.path.join(inputs, filename)
         file.save(image_path)
-
-        res_filename = myupscale(filename)
+        res_filename = ""
+        if type == 1:
+            res_filename = myupscale(filename)
+        elif type == 2:
+            res_filename = pic2anime(filename)
 
         # 现在您可以在img中访问图像数据，img_data中包含了图像的原始二进制数据
 
@@ -96,5 +117,26 @@ def upload_image():
         # return response
 
 
+class UploadImg(Resource):
+
+    # def __init__(self):
+    #     # 创建一个新的解析器
+    #     self.parser = reqparse.RequestParser()
+    #     # 增加imgFile参数，用来解析前端传来的图片。
+    #     self.parser.add_argument(
+    #         'imgFile',
+    #         required=True,
+    #         type=FileStorage,
+    #         location='files',
+    #         help="imgFile is wrong.")
+
+    def post(self, id):
+        img_file = request.files['file']
+        image_base64_str = upload_image(img_file, id)
+        return {'data': image_base64_str}
+
+
 if __name__ == '__main__':
+    api = Api(app)
+    api.add_resource(UploadImg, '/uploadImg/<int:id>')
     app.run(debug=True, host="0.0.0.0", port=5000)
